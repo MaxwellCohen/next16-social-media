c4747da37b7336ee4ee5abbfbb3d93601f9cbb0f
Aurora Scharff <aurora.sofie@gmail.com>
2026-05-20 14:35:52 +0200

    update

--- features/drop/components/feed.tsx
+++ features/drop/components/feed.tsx
@@ -1,11 +1,26 @@
 import { Drop } from '@/features/drop/components/drop';
 import { FeedList } from '@/features/drop/components/feed-list';
-import { getFeed } from '@/features/drop/drop-queries';
+import { getFeed, getPublicFeed } from '@/features/drop/drop-queries';
 
 export async function Feed() {
   const { drops, nextCursor } = await getFeed();
   return (
-    <FeedList initialCursor={nextCursor}>
+    <FeedList initialCursor={nextCursor} feedType="following">
+      {drops.map(drop => {
+        return (
+          <li key={drop.id}>
+            <Drop drop={drop} />
+          </li>
+        );
+      })}
+    </FeedList>
+  );
+}
+
+export async function PublicFeed() {
+  const { drops, nextCursor } = await getPublicFeed();
+  return (
+    <FeedList initialCursor={nextCursor} feedType="foryou">
       {drops.map(drop => {
         return (
           <li key={drop.id}>
--- features/drop/feed-actions.tsx
+++ features/drop/feed-actions.tsx
@@ -1,10 +1,24 @@
 'use server';
 
 import { Drop } from '@/features/drop/components/drop';
-import { getFeed } from '@/features/drop/drop-queries';
+import { getFeed, getPublicFeed } from '@/features/drop/drop-queries';
 
 export async function loadMoreFeed(cursor: string | null) {
   const { drops, nextCursor } = await getFeed(cursor);
+  return {
+    items: drops.map(drop => {
+      return (
+        <li key={drop.id}>
+          <Drop drop={drop} />
+        </li>
+      );
+    }),
+    nextCursor,
+  };
+}
+
+export async function loadMorePublicFeed(cursor: string | null) {
+  const { drops, nextCursor } = await getPublicFeed(cursor);
   return {
     items: drops.map(drop => {
       return (
