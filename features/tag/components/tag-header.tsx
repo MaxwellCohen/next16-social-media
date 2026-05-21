abfbe33dbd473ec92e6c1c17c8b55a4f3ffd1df3
Aurora Scharff <aurora.sofie@gmail.com>
2026-05-21 17:49:43 +0200

    fix skeleton size

--- features/tag/components/tag-header.tsx
+++ features/tag/components/tag-header.tsx
@@ -16,12 +16,9 @@ export async function TagHeader({ tag }: { tag: string }) {
 export function TagHeaderSkeleton() {
   return (
     <header className="border-divider/70 dark:border-divider-dark/70 sticky top-0 z-30 flex items-center gap-3 border-b bg-white/70 px-4 py-4 backdrop-blur-md backdrop-saturate-150 sm:px-5 dark:bg-black/70">
-      <div className="text-gray -ml-1 p-1">
-        <Skeleton className="h-5 w-5 rounded-full" />
-      </div>
       <div>
-        <Skeleton className="h-3 w-8 rounded" />
-        <Skeleton className="mt-1 h-5 w-28 rounded" />
+        <Skeleton className="h-2 w-8 rounded" />
+        <Skeleton className="mt-1 h-4 w-28 rounded" />
       </div>
     </header>
   );
