ce451b7e879f29529dd9263d6e9d94759d553d66
Aurora Scharff <aurora.sofie@gmail.com>
2026-07-23 01:37:25 +0200

    update readme

--- features/drop/components/drop-actions.tsx
+++ features/drop/components/drop-actions.tsx
@@ -104,9 +104,7 @@ export function DropActions({ dropId, parentId, replies, reposts, likes, userSta
           onClick={() => {
             toggle('bookmarked', () => toggleBookmark(dropId));
           }}
-        >
-          <span className="hidden sm:inline">{optimistic.bookmarked ? 'Saved' : 'Save'}</span>
-        </IconButton>
+        />
       </div>
     </Boundary>
   );
